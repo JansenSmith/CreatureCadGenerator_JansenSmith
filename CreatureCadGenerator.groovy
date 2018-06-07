32ed3858dd8b1887ccf4a6fd98e9104f53a1997d
Kevin Harrington <mad.hephaestus@gmail.com>
2018-06-07 17:36:45 -0400

    new loader API

--- CreatureCadGenerator.groovy
+++ CreatureCadGenerator.groovy
@@ -1,21 +1,23 @@
 import com.neuronrobotics.bowlerstudio.creature.ICadGenerator;
 import com.neuronrobotics.bowlerstudio.creature.CreatureLab;
 import org.apache.commons.io.IOUtils;
-//Create the kinematics model from the xml file describing the D-H compliant parameters. 
-def file=["https://github.com/madhephaestus/carl-the-hexapod.git","CarlTheRobot.xml"]as String[]
-String xmlContent = ScriptingEngine.codeFromGit(file[0],file[1])[0]
-
-println "Loading the robot"
-MobileBase base=null;
-if(DeviceManager.getSpecificDevice(MobileBase.class, "CarlTheWalkingRobot")==null){
-	BowlerStudio.speak("Connecting CarlTheWalkingRobot.");
-	base = new MobileBase(IOUtils.toInputStream(xmlContent, "UTF-8"));
-	DeviceManager.addConnection(base,"CarlTheWalkingRobot");
-}else{
-	base = (MobileBase)DeviceManager.getSpecificDevice(MobileBase.class, "CarlTheWalkingRobot");
-}
-
-
+def base;
+//Check if the device already exists in the device Manager
+if(args==null){
+	base=DeviceManager.getSpecificDevice( "CarlTheWalkingRobot",{
+			//If the device does not exist, prompt for the connection
+			MobileBase m = MobileBaseLoader.fromGit(
+				"https://github.com/madhephaestus/carl-the-hexapod.git",
+				"CarlTheRobot.xml"
+				)
+			if(m==null)
+				throw new RuntimeException("Arm failed to assemble itself")
+			println "Connecting new device robot arm "+m
+			return m
+		})
+}else
+	base=args.get(0)
+	
 def script = ["https://gist.github.com/e54cfebe4f55fb0549dd.git","ExampleCadGenerator.groovy"]as String[]
 base.setGitCadEngine(script)
 for(DHParameterKinematics appendge: base.getAllDHChains()){
